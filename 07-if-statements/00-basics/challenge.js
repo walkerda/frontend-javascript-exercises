aee270df0b46c9fc898845b3048546cb7df3bb6f
Daniel Walker <walkerda2010@gmail.com>
2014-07-23 07:23:12 +0000

    Solved checkLock problem

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -10,6 +10,14 @@ module.exports.favoriteNumber = function(favNum, guessNum) {
 	}
 };
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(num1, num2, num3, num4) {
+	if (((num1 === 3) || (num1 === 5) || (num1 === 7)) && (num2 === 2) && 
+		((num3 >=5) && (num3 <= 100)) && ((num4 < 9) || (num4 > 20))) {
+		return "correct";
+	}
+	else {
+		return "incorrect";
+	}
+};
 
 module.exports.canIGet = undefined;
