557ccfa31046ea8b24c2fd7b2f8fd6fe275b29a6
Daniel Walker <walkerda2010@gmail.com>
2014-07-23 07:14:32 +0000

    Solved favoriteNumber problem

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,4 +1,14 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(favNum, guessNum) {
+	if (favNum > guessNum) {
+		return "Too low";
+	}
+	else if (favNum < guessNum) {
+		return "Too high";
+	}
+	else {
+		return "You got it!";
+	}
+};
 
 module.exports.checkLock = undefined;
 
