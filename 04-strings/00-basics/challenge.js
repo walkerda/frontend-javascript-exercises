fff3a35de9da4815ba007e634e21dc2e608d92ec
Daniel Walker <walkerda2010@gmail.com>
2014-07-23 22:20:31 +0000

    Made a commit because this was unmerged

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -7,6 +7,7 @@ module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, e
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
+<<<<<<< Updated upstream
 
 };
 
@@ -16,4 +17,16 @@ module.exports.findLastMatch = function(text, searchString) {
 
 module.exports.substringBetweenMatches = function(text, searchString) {
 
+=======
+	return text.indexOf(searchString);
+};
+
+module.exports.findLastMatch = function(text, searchString) {
+	return text.lastIndexOf(searchString);
+};
+
+module.exports.substringBetweenMatches = function(text, searchString) {
+	return text.substring(text.lastIndexOf(searchString), 10) + 
+		text.substring(text.lastIndexOf(searchString), 13);
+>>>>>>> Stashed changes
 };
