3a8dc3f7515586388057fdcdb33d0c849f793f63
Daniel Walker <walkerda2010@gmail.com>
2014-07-28 07:24:32 +0000

    Updated stash to merge

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,51 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(title, duration, students) {
+	// var course = {
+	// 	title: title,
+	// 	duration: duration,
+	// 	students: students
+	// };
 
-module.exports.addProperty = undefined;
+	// return course;
 
-module.exports.formLetter = undefined;
+	var course = {};
+	course.title = title;
+	course.duration = duration;
+	course.students = students;
+	
+	return course; 
+};
 
-module.exports.canIGet = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+	//object[newProp] = newValue;
+	//bracket notation works...?
+
+	//object.newProp = newValue;
+	//why doesn't dot notation work???
+	
+	var newObject = object;
+	newObject[newProp] = newValue;
+
+	return newObject;
+};
+
+module.exports.formLetter = function(letter) {
+	var letterInfo = {
+		recipient: letter.recipient,
+		sender: letter.sender,
+		msg: letter.msg
+	};
+
+	var letterStr = 'Hello ' + letterInfo.recipient + ',\n\n' + letterInfo.msg + '\n\nSincerely,\n' + letterInfo.sender;
+
+	return letterStr;
+};
+
+module.exports.canIGet = function(item, money) {
+	var priceTable = {};
+
+
+<<<<<<< Updated upstream
+};
+=======
+};
+>>>>>>> Stashed changes
