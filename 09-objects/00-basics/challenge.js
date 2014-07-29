7ed602a1555666784110b02ead9deba2a43cb03d
Daniel Walker <walkerda2010@gmail.com>
2014-07-29 05:54:45 +0000

    Still working on canIGet problem of Unit9_exercises

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,21 +1,55 @@
 module.exports.createCourse = function(title, duration, students) {
-	var course = {
-		title: title,
-		duration: duration,
-		students: students
-	};
+	//OPTION 1
+	// var course = {
+	// 	title: title,
+	// 	duration: duration,
+	// 	students: students
+	// };
 
-	return course;
+	// return course;
 
-	// var course = {};
-	// course.title = title;
-	// course.duration = duration;
-	// course.students = students;
-	// return course; 
+	//OPTION 2
+	var course = {};
+	course.title = title;
+	course.duration = duration;
+	course.students = students;
+	
+	return course; 
 };
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+	//OPTION 1
+	//object[newProp] = newValue;
 
-module.exports.formLetter = undefined;
+	//object.newProp = newValue;
+	//why doesn't dot notation work???
+	
+	//OPTION 2
+	var newObject = object;
+	newObject[newProp] = newValue;
 
-module.exports.canIGet = undefined;
+	return newObject;
+};
+
+module.exports.formLetter = function(letter) {
+	var letterInfo = {
+		recipient: letter.recipient,
+		sender: letter.sender,
+		msg: letter.msg
+	};
+
+	var letterStr = 'Hello ' + letterInfo.recipient + ',\n\n' + letterInfo.msg + '\n\nSincerely,\n' + letterInfo.sender;
+
+	return letterStr;
+};
+
+module.exports.canIGet = function(item, money) {
+	var cost = {
+		item: item,
+		amount: money
+	};
+
+	console.log(Boolean(cost));
+	
+	return Boolean(cost);
+};
