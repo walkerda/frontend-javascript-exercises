5d3fb0be0202835822dcfad26735082b2e2c504d
Daniel Walker <walkerda2010@gmail.com>
2014-07-30 01:10:30 +0000

    Still working on the last problem of the 00-looping-over-arrays exercises

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,21 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array) {
+	var total = 0;
+	for (var i = 0; i < array.length; i++) {
+		total += array[i];
+	}
+	return total;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+	return inputString.toLowerCase().split(",");
 
-module.exports.addIndex = undefined;
+};
+
+module.exports.addIndex = function(inputData) {
+	var outputData = [];
+	for (var i = 0, len = inputData.length; i < len; i++) {
+	outputData += i + ' is ' + inputData[i];
+	}
+	console.log(outputData.split());
+	return outputData.split();
+};
