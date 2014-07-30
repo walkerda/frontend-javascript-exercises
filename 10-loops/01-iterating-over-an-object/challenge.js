43f408394163f8d50f2cd40dea28d88c7135e33e
Daniel Walker <walkerda2010@gmail.com>
2014-07-30 13:32:03 +0000

    Solved problems for 01-iterating-over-an-object

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,14 +1,28 @@
 module.exports.getKeys = function(object) {
-	for (var property in object) {
-		console.log(property + object[property]);
-		return property + object[property];
+	
+	var keys = [];
+	for (var key in object) {
+		keys.push(key);
+		console.log(keys);
 	}
+	return keys;
 };
 
-module.exports.getValues = function() {
-
+module.exports.getValues = function(object) {
+	var values = [];
+	for (var value in object) {
+		values.push(object[value]);
+		console.log(values);
+	}
+	return values;
 };
 
-module.exports.objectToArray = function() {
+module.exports.objectToArray = function(object) {
+	var result = [];
+	for (var key in object) {
+		result.push(key + " is " + object[key]);
+	}
+	console.log(result);
+	return result;
 
 };
