63f057841f46f272378e8bca9b8dc9e32d09996b
Daniel Walker <walkerda2010@gmail.com>
2014-07-31 05:32:40 +0000

    Sitll working on 02-object-operations

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,19 @@
-module.exports.copy = undefined;
+module.exports.copy = function(object) {
+	var newObject = {};
+	for (var key in object) {
+		newObject[key] = object[key];
+	}
+	return newObject;
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src) {
+	for (var prop in dest) {
+			src[prop] = dest[prop];
+		}
+	console.log(dest);
+	return dest;
+};
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(object, array) {
+
+};
