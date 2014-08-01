f1ae92f1ca8c13de7a3a94e0c8ddcc83cf938325
Daniel Walker <walkerda2010@gmail.com>
2014-08-01 05:42:16 +0000

    Solved problems for 03-while-loops

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,20 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn) {
+	
+	while (conditionalFn()) {
+		actionFn();
+	}
+};
 
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array) {
+	var i = 0;
+	var total = 0;
+	while (i < array.length) {
+		total += array[i];
+		i++;
+	}
+	return total;
+
+};
+
+
 
