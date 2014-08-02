8f2a0f0791ffbcfa7757af088b6ac296e14d86ac
Daniel Walker <walkerda2010@gmail.com>
2014-08-02 02:13:47 +0000

    Still working on the last problem for 02-object-operations

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -7,13 +7,20 @@ module.exports.copy = function(object) {
 };
 
 module.exports.extend = function(dest, src) {
-	for (var prop in dest) {
-			src[prop] = dest[prop];
-		}
-	console.log(dest);
+	for (var prop in src) {
+     dest[prop] = src[prop];
+   }
 	return dest;
 };
 
 module.exports.hasElems = function(object, array) {
+	var newObject = {};
+
+	for (var key in array) {
+		newObject[key] = array[key];
+	}
+	console.log(newObject[key]);
+	console.log(newObject[key] === object[key]);
+	return newObject[key] === object[key];
 
 };
